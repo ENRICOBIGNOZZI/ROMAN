ee1cdcfe094e4f08619f2054dd5d648037b45ed2
ENRICOBIGNOZZI <88671442+ENRICOBIGNOZZI@users.noreply.github.com>
2026-08-23 11:03:35 +0200

    Expose pure arbitrage engine

--- src/roman_arb/__init__.py
+++ src/roman_arb/__init__.py
@@ -1,1 +1,9 @@
 __version__ = "0.1.0"
+
+from .pure_arbitrage import ArbitrageLeg, PureArbitrageEngine, PureArbitrageOpportunity
+
+__all__ = [
+    "ArbitrageLeg",
+    "PureArbitrageEngine",
+    "PureArbitrageOpportunity",
+]
